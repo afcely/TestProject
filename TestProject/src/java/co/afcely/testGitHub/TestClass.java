1f01ae7a09889be68cd5af11330ba176351f30f7
afcely <afcely@gmail.com>
2017-05-17 15:42:08 -0500

    Creacion de la clase Test

---
+++ TestProject/src/java/co/afcely/testGitHub/TestClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.afcely.testGitHub;
+
+/**
+ *
+ * @author acelyv
+ */
+public class TestClass {
+   
+   private String name;
+   private int key;
+
+   public TestClass( String name, int key ) {
+      this.name = name;
+      this.key = key;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public int getKey() {
+      return key;
+   }
+   
+   
+   
+}
